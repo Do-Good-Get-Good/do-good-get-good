65b917403c0946c427c109113181f69ac10f84ed
Mattan1999 <mattias470@gmail.com>
2021-09-15 15:17:12 +0200

    Creating Login.js
    - Creating the design for the login page and added as start page
    - Adding a background image and two logos
    - DGGG logo is temporary

---
+++ src/views/Login.js
@@ -0,0 +1,142 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View, TextInput, StyleSheet, Pressable, ImageBackground, Image } from 'react-native';
+
+export default function Login() {
+
+  const [email, setEmail] = React.useState('');
+  const [pass, setPass] = React.useState('');
+
+
+  return (
+    <ImageBackground 
+    source={require('../img/blueprint - white.png')}
+    resizeMode={'cover'} 
+    style={{
+      flex: 1
+    }}>
+      <View style={{
+       backgroundColor: '#00000010',
+      }}>
+        <Image source={require('../img/Logo-DGGG-13.png')} style={{width: '100%', height: '250px'}} />
+      </View>
+      <View
+        style={{
+          padding: 20,
+          flex: 1,
+          backgroundColor: '#00000010',
+        }}
+      >
+        <Text style={{
+          alignSelf: 'center',
+          fontSize: 24,
+          marginBottom: 50,
+          color: '#333333',
+          fontWeight: '600',
+        }}>Du är riktigt grym!
+        </Text>
+        <View style={{
+          flexDirection: 'row',
+          marginBottom: 20,
+        }}>
+            <TextInput
+              textContentType={'emailAddress'}
+              onChangeText={(text) => setEmail(text)}
+              value={email}
+              keyboardType={'email-address'}
+              placeholder={'E-mail'}
+              style={styles.inputField}
+            />
+        </View>
+        <View style={{
+          flexDirection: 'row',
+          marginBottom: 20,
+        }}>
+          <TextInput
+            textContentType={'password'}
+            onChangeText={(text) => setPass(text)}
+            value={pass}
+            placeholder={'Lösenord'}
+            secureTextEntry={true}
+            style={styles.inputField}
+          />
+        </View>
+        <View style={{
+          alignSelf: 'center',
+          marginTop: 25,
+          width: '100%',
+        }}>
+          <Pressable style={{
+            backgroundColor: '#84BD00', 
+            height: 55,
+            justifyContent: 'center',
+            borderRadius: 5
+          }} onPress={() => {console.log("Tryckte på 'logga in'")}}>
+            <Text style={{
+              color: '#333333',
+              alignSelf: 'center',
+              fontSize: 20,
+              fontWeight: '400'
+            }}>
+              Logga in
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{
+          marginTop: 20,
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'center'
+        }}>
+          <Text style={{
+              color: '#333333',
+              fontSize: 14,
+              fontWeight: '400'
+            }}>
+              Glömt ditt lösenord? 
+            </Text>
+          <Pressable style={{
+            backgroundColor: '', 
+            justifyContent: 'center',
+            marginLeft: 5,
+          }} onPress={() => {console.log("Tryckte på 'glömt lösenord'")}}>
+            <Text style={{
+              color: '#84BD00',
+              fontSize: 14,
+              fontWeight: '400',
+              textDecorationLine: 'underline'
+            }}>Tryck här
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+      <View style={{
+       backgroundColor: '#00000010',
+       width: '100%',
+      //  flex: 1
+      }}>
+        <Image 
+          source={require('../img/Technogarden-logotyp-Large.png')} 
+          style={{
+            width: '143px', 
+            height: '22.93px', 
+            alignSelf: 'center',
+            marginBottom: 10
+          }} />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputField: {
+    flex: 1,
+    height: 49,
+    backgroundColor: '#FFFFFF',
+    paddingLeft: 15,
+    fontWeight: '500',
+    borderRadius: 5,
+    fontSize: 18,
+    color: '#333333'
+  },
+});
