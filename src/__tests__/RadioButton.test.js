29f6933bf873243050791107a45739c35e15741a
Alona-Kirichenko <alena1412199217@gmail.com>
2022-02-02 13:22:49 +0100

    Doing RadioButton test for send true/false value to context. Changing description

--- src/__tests__/RadioButton.test.js
+++ src/__tests__/RadioButton.test.js
@@ -3,40 +3,47 @@ import React from "react";
 import { render, fireEvent, act, } from "@testing-library/react-native";
  
 import RadioButton from "../components/RadioButton"
+import { useAdminGalleryFunction} from "../context/AdminGalleryContext";
+
+
 
 jest.mock('../context/AdminGalleryContext', () => ({
     
     useAdminGalleryFunction: () => ({
         chooseActiveOrNot: jest.fn()
     })
+
   }));
 
-  
 describe('Testing Radio Button', () => {
 
 
-    it("can find RadioButton", () => {
+    it("can find RadioButton Ja", () => {
         const { getAllByText } = render(<RadioButton />);
         expect(getAllByText(" Ja ").length).toBe(1);
        
     })
 
 
-    it("can find RadioButton", () => {
+    it("can find RadioButton Nej", () => {
         const { getAllByText } = render(<RadioButton />);
         expect(getAllByText(" Nej ").length).toBe(1);
     })
 
 
-    it("can press the button RadioButton Ja", () => {
+    it("can press the button RadioButton Ja and send value true to context", () => {
+     useAdminGalleryFunction().chooseActiveOrNot.mockReturnValue(true)
         const { getByTestId } = render(<RadioButton />);
         const buttonJa = getByTestId("pressOnButtonJa")
         fireEvent.press(buttonJa)
+      
+       
     })
 
    
 
-    it("can press the button RadioButton Nej", () => {
+    it("can press the button RadioButton Nej and send value false to context", () => {
+         useAdminGalleryFunction().chooseActiveOrNot.mockReturnValue(false) 
         const { getByTestId } = render(<RadioButton />);
         const buttonNej = getByTestId("pressOnButtonNej")
         fireEvent.press(buttonNej)
