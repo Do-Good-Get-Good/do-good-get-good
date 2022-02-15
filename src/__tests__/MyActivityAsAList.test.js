b2d74da8ecdf6d45d738f304dab868c346f4d1be
Jimmy Meriläinen <merilainen@live.se>
2022-02-15 15:59:39 +0100

    Updating tests for MyActivityAsAList

--- src/__tests__/MyActivityAsAList.test.js
+++ src/__tests__/MyActivityAsAList.test.js
@@ -2,9 +2,6 @@ import 'react-native'
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react-native'
 import { MyActivityAsAList } from '../components/MyActivityAsAList';
-import { useActivityFunction } from '../context/ActivityContext'
-import toDate from 'date-fns/toDate'
-import { useRoute } from '@react-navigation/native'
 
 // jest.mock("@react-native-firebase/firestore", () => {
 //     return () => ({
@@ -16,22 +13,29 @@ import { useRoute } from '@react-navigation/native'
 //                         set: jest.fn(),
 //                         delete: jest.fn()
 //                     })),
-//                     get: jest.fn(() => ({
-//                         data: jest.fn().mockReturnValue({
-//                             date: {
-//                                 toDate: () => '2022-01-27T00:00:00.000Z',
-//                             }
-//                         })
-//                     }))
+//                     orderBy: jest.fn(() => ({
+//                         get: jest.fn(() => ({
+//                             data: jest.fn().mockReturnValue({
+//                                 date: {
+//                                     toDate: () => '2022-01-27T00:00:00.000Z',
+//                                 },
+//                                 activityId: 'asd', 
+//                                 fbDocumentID: "z7kknsEWFeJPhHPev2lA", 
+//                                 statusConfirmed: false, 
+//                                 time: 1.5,
+//                                 // id: 'asd',
+//                                 // title: 'Missing people',
+//                                 // city: 'Gbg',
+//                                 // photo: 'symbol_earth',
+//                             })
+//                         }))
+//                     }))                   
 //                 }))
 //             }))
 //         }))
 //     });
 // });
 
-// jest.mock("@react-native-firebase/auth", () => () => ({
-//     auth: jest.fn(),
-// }))
 
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter")
 
@@ -43,30 +47,6 @@ jest.mock('../components/CalendarView', () => () => {
     return <fakeCalenderView />
 })
 
-const mockedNavigate = jest.fn()
-
-// jest.mock('@react-navigation/native', () => {
-//     const actualNav = jest.requireActual('@react-navigation/native');
-//       return {
-//         ...actualNav,
-//         useNavigation: () => ({
-//           navigate: mockedNavigate,
-//         }),
-//     };
-// });
-
-// jest.mock('@react-navigation/native', () => {
-//     const actualNav = jest.requireActual('@react-navigation/native');
-//       return {
-//         ...actualNav,
-//         // useRoute: () => mockedNavigate
-//         useRoute: () => ({
-//             name: mockedNavigate2,
-//         }),
-//     };
-// });
-
-
 jest.mock('@react-navigation/native')
 
 jest.mock('../context/ActivityContext', () => ({
@@ -77,17 +57,6 @@ jest.mock('../context/ActivityContext', () => ({
         getIfoFromActivitiesList: jest.fn()
       }),
 }))
-
-
-
-// const timeandstatusContext = [{
-//     activityId: 'asd', 
-//     date: '2022-01-27T00:00:00.000Z', 
-//     fbDocumentID: "z7kknsEWFeJPhHPev2lA", 
-//     statusConfirmed: false, 
-//     time: 1.5
-// }]
-// let currentDate = new Date(timestamp)
 
 // jest.mock('../context/ActivityContext', () => {
 //     const timeandstatusContext = [{
@@ -114,10 +83,17 @@ jest.mock('../context/ActivityContext', () => ({
 
 const showAllList = [{
     activityId: 'asd', 
-    date: {nanoseconds: 0, seconds: 1643241600}, 
+    date: {nanoseconds: 0, seconds: 1643241600},
     fbDocumentID: "z7kknsEWFeJPhHPev2lA", 
     statusConfirmed: false, 
     time: 1.5
+},
+{
+    activityId: 'abc', 
+    date: {nanoseconds: 0, seconds: 1643241600},
+    fbDocumentID: "z7kknsEWFeJPhHPev2lB", 
+    statusConfirmed: false, 
+    time: 1
 }]
 
 const useStateMock = [{
@@ -130,42 +106,33 @@ const useStateMock = [{
 React.useState = jest.fn().mockReturnValue([useStateMock, {}])
 
 describe('Testing MyActivityAsAList', () => {
-    console.log(new Date().toISOString().slice(0, 10))
     it('can find the text Min tid', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'HomePage'})
-        // require('@react-navigation/native').useRoute({name: 'HomePage'})
-        // require(useRoute).mockReturnValue('HomePage')
         const { getAllByText } = render(<MyActivityAsAList />)
         expect(getAllByText('Min tid').length).toBe(1)
     })
 
     it('can find the activity title', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'HomePage'})
-        // require('@react-navigation/native').useRoute({name: 'HomePage'})
-        const { getAllByText, getByTestId } = render(<MyActivityAsAList />)
+        const { getAllByText } = render(<MyActivityAsAList />)
         expect(getAllByText('Missing people').length).toBe(1)
     })
 
     it('can find the activity date', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'HomePage'})
-        // require('@react-navigation/native').useRoute({name: 'HomePage'})
-        
         const { getAllByText } = render(<MyActivityAsAList />)   
         expect(getAllByText(new Date().toISOString().slice(0, 10)).length).toBe(1)
+        // expect(getAllByText('2022-01-27').length).toBe(1)
     })
 
     it('can find the activity time', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'HomePage'})
-        // require('@react-navigation/native').useRoute({name: 'HomePage'})
-        
         const { getAllByText } = render(<MyActivityAsAList />)   
         expect(getAllByText('1.5 tim').length).toBe(1)  
     })
 
     it('can press the edit button if statusConfirmed is false', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'HomePage'})
-        // require('@react-navigation/native').useRoute({name: 'HomePage'})
-        
         const { getByTestId } = render(<MyActivityAsAList />)   
         const button = getByTestId('editButton') 
         fireEvent.press(button)
@@ -190,21 +157,20 @@ describe('Testing MyActivityAsAList', () => {
 
     it('can press on the button Visa allt in HomePage screen', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'HomePage'})
-
-        // require(mockedNavigate2).useRoute.name.mockReturnValue('HomePage')
-        // require('@react-navigation/native').useRoute({name: 'HomePage'})
-        const { getAllByText, getByTestId } = render(<MyActivityAsAList navigation={mockedNavigate}/>)
+       
+        const navigate = jest.fn()
+    
+        const { getAllByText, getByTestId, queryByText } = render(<MyActivityAsAList navigation={{navigate}} />)
         expect(getAllByText('Visa allt').length).toBe(1)
         const button = getByTestId('showAllButton')
-        // fireEvent.press(button)
-        // expect(mockedNavigate).toHaveBeenCalledWith('MyTimePage')
+        fireEvent.press(button)
+        expect(navigate).toHaveBeenCalledWith('MyTimePage')
     })
-
 
     it('hide button Visa allt in MyTimePage screen', () => {
         require('@react-navigation/native').useRoute.mockReturnValue({name: 'MyTimePage'})
 
-        const { queryByText} = render(<MyActivityAsAList navigation={mockedNavigate} showAllList={showAllList}/>)
+        const { queryByText} = render(<MyActivityAsAList showAllList={showAllList}/>)
         expect(queryByText('Visa allt')).toBeNull()
     })
 
