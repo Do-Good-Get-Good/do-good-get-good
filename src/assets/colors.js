8a92638d06185a51fa2dae70118397e1171b32f3
Mattan1999 <mattias470@gmail.com>
2022-02-24 20:20:22 +0100

    Adding colors and typography styling from Figma

---
+++ src/assets/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  primary: "#84BD00",
+  secondary: "#5B6770",
+  dark: "#333333",
+  light: "#F5F5F5",
+  background: "#FFFFFF",
+  error: "#C62F25",
+  gradient: {
+    primary: "#84BD00",
+    secondary: "#5B6770",
+  },
+};
+
+export default colors;
