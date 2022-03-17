fce66cac30d21c499e48ac004984784337a2afbc
Mattan1999 <mattias470@gmail.com>
2022-03-17 13:28:13 +0100

    Info modal (#86)
    * Adding InfoModal to homepage and image gallery
    
    * Removing unused imports and changing text
    
    * Removing InfoModal from user homepage and updating package-lock

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,21 +1,14 @@
 import React from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  View,
-  ScrollView,
-  Image,
-  Text,
-} from "react-native";
-import { MyActivities } from "../components/MyActivities";
-import { MyActivityAsAList } from "../components/MyActivityAsAList";
-import { Suggestions } from "../components/Suggestions";
-
-import Menu from "../components/Menu";
+import { StyleSheet, SafeAreaView, View, ScrollView, Text } from "react-native";
 
 import { useActivityFunction } from "../context/ActivityContext";
 import { useAdminCheckFunction } from "../context/AdminContext";
 import { SuggestionProvider } from "../context/SuggestionContext";
+
+import Menu from "../components/Menu";
+import { MyActivities } from "../components/MyActivities";
+import { MyActivityAsAList } from "../components/MyActivityAsAList";
+import { Suggestions } from "../components/Suggestions";
 import FloatingActionButton from "../components/FloatingActionButton";
 import ConfirmActivities from "../components/ConfirmActivities";
 import MyUsers from "../components/MyUsers";
@@ -29,7 +22,7 @@ export const HomePage = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.view}>
       <Menu />
-      {userLevel === "admin" ? (
+      {userLevel === "admin" && (
         <>
           <ScrollView style={styles.container}>
             <ConfirmActivities />
@@ -38,8 +31,8 @@ export const HomePage = ({ navigation }) => {
           </ScrollView>
           <FloatingActionButton />
         </>
-      ) : null}
-      {userLevel === "user" ? (
+      )}
+      {userLevel === "user" && (
         <>
           {activity.myActivities.length != 0 ? (
             <View style={styles.view}>
@@ -67,10 +60,11 @@ export const HomePage = ({ navigation }) => {
                 <Suggestions navigation={navigation} />
               </SuggestionProvider>
               <MyActivityAsAList navigation={navigation} />
+              <BottomLogo />
             </ScrollView>
           )}
         </>
-      ) : null}
+      )}
     </SafeAreaView>
   );
 };
