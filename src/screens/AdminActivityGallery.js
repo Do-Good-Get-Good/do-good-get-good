d2f62e55967a494dc2c402d6f94e60aec5833412
Jimmy Meriläinen <merilainen@live.se>
2022-04-25 16:47:11 +0200

    Added keyboardDismissMode. When scrolling the keyboard will dismiss

--- src/screens/AdminActivityGallery.js
+++ src/screens/AdminActivityGallery.js
@@ -6,6 +6,8 @@ import {
   ScrollView,
   Button,
   Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -152,6 +154,7 @@ export function AdminActivityGallery({ navigation }) {
       <Menu />
       <ScrollView
         keyboardShouldPersistTaps={"always"}
+        keyboardDismissMode={"on-drag"}
         style={{ paddingHorizontal: 16 }}
       >
         <Text style={styles.headerText}>Aktivitetsgalleri</Text>
