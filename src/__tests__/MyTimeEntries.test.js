c11a67133ff067417d611d9d38026ebf30958062
Mattan1999 <mattias470@gmail.com>
2022-12-15 15:16:35 +0100

    Fix MyTimeEntries test

--- src/__tests__/MyTimeEntries.test.js
+++ src/__tests__/MyTimeEntries.test.js
@@ -2,6 +2,7 @@ import "react-native";
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import MyTimeEntries from "../components/MyTimeEntries";
+import useTimeEntriesWithLimit from "../customFirebaseHooks/useTimeEntriesWithLimit";
 
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 
@@ -22,25 +23,46 @@ jest.mock("../components/DropDownForSorting", () => () => {
 
 jest.mock("@react-navigation/native");
 
-jest.mock("../customFirebaseHooks/useTimeEntriesWithLimit", () => () => ({
-  useTimeEntriesWithLimit: () => ({
-    timeEntries: [
-      {
-        adminID: "123",
-        timeEntryID: "123",
-        date: new Date(),
-        statusConfirmed: false,
-        time: 2,
-        title: "title",
-        activityID: "123",
-      },
-    ],
-    isLoading: false,
-    error: null,
-  }),
-}));
+jest.mock("../customFirebaseHooks/useTimeEntriesWithLimit");
+
+// ({
+//   __esModule: true,
+//   useTimeEntriesWithLimit: () => ({
+//     timeEntries: [
+//       {
+//         adminID: "123",
+//         timeEntryID: "123",
+//         date: new Date(),
+//         statusConfirmed: false,
+//         time: 2,
+//         title: "title",
+//         activityID: "123",
+//       },
+//     ],
+//     isLoading: false,
+//     error: null,
+//   }),
+// });
 
 describe("Testing MyTimeEntries", () => {
+  beforeAll(() => {
+    useTimeEntriesWithLimit.mockReturnValue({
+      timeEntries: [
+        {
+          adminID: "123",
+          timeEntryID: "123",
+          date: new Date(),
+          statusConfirmed: false,
+          time: 2,
+          title: "Missing people",
+          activityID: "123",
+        },
+      ],
+      isLoading: false,
+      error: null,
+    });
+  });
+
   it("can find the text Min tid", () => {
     const { getAllByText } = render(<MyTimeEntries />);
     expect(getAllByText("Mina aktiviteter").length).toBe(1);
@@ -48,17 +70,17 @@ describe("Testing MyTimeEntries", () => {
 
   it("can find the activity title", () => {
     const { getAllByText } = render(<MyTimeEntries />);
-    expect(getAllByText("Missing people").length).toBe(2);
+    expect(getAllByText("Missing people").length).toBe(1);
   });
 
   it("can find the activity date", () => {
     const { getAllByText } = render(<MyTimeEntries />);
-    expect(getAllByText(new Date().toISOString().slice(0, 10)).length).toBe(2);
+    expect(getAllByText(new Date().toISOString().slice(0, 10)).length).toBe(1);
   });
 
   it("can find the activity time", () => {
     const { getAllByText } = render(<MyTimeEntries />);
-    expect(getAllByText("1.5 tim").length).toBe(2);
+    expect(getAllByText("2 tim").length).toBe(1);
   });
 
   it("can press the edit button if statusConfirmed is false", () => {
