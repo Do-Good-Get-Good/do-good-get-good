078aaa72a1e01e3c782d53d63a5986f80747e5ef
Mattan1999 <mattias470@gmail.com>
2022-12-28 14:13:39 +0100

    Remove unused import

--- src/__tests__/DatePicker.test.js
+++ src/__tests__/DatePicker.test.js
@@ -1,5 +1,5 @@
 import "react-native";
-import React, { useState } from "react";
+import React from "react";
 import { fireEvent, render } from "@testing-library/react-native";
 
 import { format } from "date-fns";
