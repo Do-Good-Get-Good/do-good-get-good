a488deed30db7537b55373e52fd1e6664e658b09
Mattan1999 <mattias470@gmail.com>
2022-12-28 13:30:31 +0100

    Create BottomNavButtons test

---
+++ src/__tests__/BottomNavButtons.test.js
@@ -0,0 +1,26 @@
+import "react-native";
+import React from "react";
+import { fireEvent, render } from "@testing-library/react-native";
+
+import BottomNavButtons from "../components/BottomNavButtons";
+
+describe("Testing BottomNavButtons", () => {
+  it("Can find primary and secondary button and press them", () => {
+    const { getByText } = render(
+      <BottomNavButtons
+        primaryText="Primary"
+        secondaryText="Secondary"
+        primaryFunc={() => {}}
+        secondaryFunc={() => {}}
+      />
+    );
+    const primaryButton = getByText("Primary");
+    const secondaryButton = getByText("Secondary");
+
+    expect(primaryButton);
+    expect(secondaryButton);
+
+    fireEvent.press(primaryButton);
+    fireEvent.press(secondaryButton);
+  });
+});
