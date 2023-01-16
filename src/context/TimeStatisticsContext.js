0c14b5ae04cef0299e502252a6df2c73f61d24c7
Mattan1999 <mattias470@gmail.com>
2023-01-16 12:14:46 +0100

    Add TimeStatisticsSettingsContext

---
+++ src/context/TimeStatisticsContext.js
@@ -0,0 +1,30 @@
+import React, { useState, useContext, useEffect } from "react";
+import auth from "@react-native-firebase/auth";
+import { getMaxConfirmedHours } from "../firebase-functions/get";
+
+const TimeStatisticsContext = React.createContext();
+
+export const useTimeStatisticSettings = () => {
+  return useContext(TimeStatisticsContext);
+};
+
+export const TimeStatisticsProvider = ({ children }) => {
+  const [maxConfirmedHours, setMaxConfirmedHours] = useState(null);
+
+  const fetchTimeStatisticsSetting = async () => {
+    const maxAmount = await getMaxConfirmedHours();
+    setMaxConfirmedHours(maxAmount);
+  };
+
+  useEffect(() => {
+    fetchTimeStatisticsSetting();
+  }, []);
+
+  return (
+    <TimeStatisticsContext.Provider
+      value={{ maxConfirmedHours: maxConfirmedHours }}
+    >
+      {children}
+    </TimeStatisticsContext.Provider>
+  );
+};
