246ccba0085ba44c4c5cc7a7acdadf918a8a427f
Mattan1999 <mattias470@gmail.com>
2023-02-27 16:43:01 +0100

    Remove old files

--- src/assets/recyclingStyles/errorMessage.js
+++
@@ -1,6 +0,0 @@
-import colors from "../theme/colors";
-const errorText = {
-  fontSize: 20,
-  color: colors.error,
-};
-export default errorText;
--- src/lib/enums/routes.js
+++
@@ -1,4 +0,0 @@
-export const Routes = {
-  HomePage: "HomePage",
-  AdminPage: "AdminPage",
-};
