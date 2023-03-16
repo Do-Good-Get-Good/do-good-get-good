6c8c00d7fbe85468ad7294bc1d6db3a1b1994de3
Mattan1999 <mattias470@gmail.com>
2023-03-16 16:12:58 +0100

    Add function to return photo or placeholder

--- src/lib/images.js
+++ src/lib/images.js
@@ -1,23 +1,37 @@
+import { Format } from "./enums/imageFormat";
+
 export const placeholderImage = require("../assets/images/placeholder.png");
 
-export default Images = [
+export function setTheRightPhoto(activityImage, format) {
+  let images = Images.filter((image) => image.format === format);
+  let found = images.find((image) => image.name === activityImage);
+
+  if (!found) return placeholderImage;
+
+  return found.image;
+}
+
+const Images = [
   {
     key: "1",
     image: require("../assets/images/activities/square/blodgivning_400x400.png"),
     name: "blodgivning",
     selected: false,
+    format: Format.square,
   },
   {
     key: "2",
     image: require("../assets/images/activities/square/djurambulans_400x400.png"),
     name: "djurambulans",
     selected: false,
+    format: Format.square,
   },
   {
     key: "3",
     image: require("../assets/images/activities/square/fotbollstranare_400x400.png"),
     name: "fotbollstranare",
     selected: false,
+    format: Format.square,
   },
 
   {
@@ -25,24 +39,28 @@ export default Images = [
     image: require("../assets/images/activities/square/friluftsliv-kajak_400x400.png"),
     name: "friluftsliv-kajak",
     selected: false,
+    format: Format.square,
   },
   {
     key: "5",
     image: require("../assets/images/activities/square/friluftsliv-unga_400x400.png"),
     name: "friluftsliv-unga",
     selected: false,
+    format: Format.square,
   },
   {
     key: "6",
     image: require("../assets/images/activities/square/frisor_400x400.png"),
     name: "frisor",
     selected: false,
+    format: Format.square,
   },
   {
     key: "7",
     image: require("../assets/images/activities/square/internationellt_400x400.png"),
     name: "internationellt",
     selected: false,
+    format: Format.square,
   },
 
   {
@@ -50,33 +68,35 @@ export default Images = [
     image: require("../assets/images/activities/square/rasta_hund_400x400.png"),
     name: "rasta_hund",
     selected: false,
+    format: Format.square,
   },
   {
     key: "9",
     image: require("../assets/images/activities/square/soppkok_400x400.png"),
     name: "soppkok",
     selected: false,
+    format: Format.square,
   },
   {
     key: "10",
     image: require("../assets/images/activities/wide/blodgivning_700x400.png"),
     name: "blodgivning",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
   {
     key: "11",
     image: require("../assets/images/activities/wide/djurambulans_700x400.png"),
     name: "djurambulans",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
   {
     key: "12",
     image: require("../assets/images/activities/wide/fotbollstranare_700x400.png"),
     name: "fotbollstranare",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
 
   {
@@ -84,28 +104,28 @@ export default Images = [
     image: require("../assets/images/activities/wide/friluftsliv-kajak_700x400.png"),
     name: "friluftsliv-kajak",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
   {
     key: "14",
     image: require("../assets/images/activities/wide/friluftsliv-unga_700x400.png"),
     name: "friluftsliv-unga",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
   {
     key: "15",
     image: require("../assets/images/activities/wide/frisor_700x400.png"),
     name: "frisor",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
   {
     key: "16",
     image: require("../assets/images/activities/wide/internationellt_700x400.png"),
     name: "internationellt",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
 
   {
@@ -113,13 +133,15 @@ export default Images = [
     image: require("../assets/images/activities/wide/rasta_hund_700x400.png"),
     name: "rasta_hund",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
   {
     key: "18",
     image: require("../assets/images/activities/wide/soppkok_700x400.png"),
     name: "soppkok",
     selected: false,
-    wide: true,
+    format: Format.wide,
   },
 ];
+
+export default Images;
