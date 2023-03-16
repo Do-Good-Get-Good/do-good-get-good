31a49e76593079b7fa980a3217cb9d12b671902d
Mattan1999 <mattias470@gmail.com>
2023-03-16 16:15:25 +0100

    Use BottomNavButtons component and Format enum

--- src/screens/ImagesGallery.js
+++ src/screens/ImagesGallery.js
@@ -17,10 +17,12 @@ import Images from "../lib/images";
 import typography from "../assets/theme/typography";
 import colors from "../assets/theme/colors";
 import InfoModal from "../components/InfoModal";
+import { Format } from "../lib/enums/imageFormat";
+import BottomNavButtons from "../components/BottomNavButtons";
 
 export function ImagesGallery({ navigation, route }) {
   const [imagesArray, setImagesArray] = useState(
-    Images.filter((img) => img.wide !== true),
+    Images.filter((img) => img.format === Format.square),
   );
   const [imageName, setImageName] = useState(route.params.selectedImage);
 
@@ -77,58 +79,35 @@ export function ImagesGallery({ navigation, route }) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Menu />
-      <View style={{ paddingHorizontal: 16, flex: 1 }}>
-        <View style={styles.headerView}>
-          <Text style={styles.mainText}>Bildgalleri</Text>
-          <InfoModal screen="imageGallery" tooltipWidth={200} />
-        </View>
-        <FlatList
-          style={styles.container}
-          columnWrapperStyle={{ marginBottom: 10 }}
-          data={imagesArray}
-          numColumns={2}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              testID="pressOnImage"
-              onPress={() => setImageName(item.name)}
-              style={{
-                flex: 0.5,
-                flexDirection: "row",
-              }}
-            >
-              <Image
-                testID="imageInImageGallery"
-                style={imageStyle(item.selected)}
-                source={item.image}
-              />
-            </TouchableOpacity>
-          )}
-        />
-        <View style={styles.containerForTwoBottomButtons}>
-          <TouchableOpacity
-            testID="saveButton"
-            onPress={() => buttonSavePressed()}
-            style={styles.saveButton}
-          >
-            <Text style={styles.saveButtonText}>Spara</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            testID="backButton"
-            onPress={() => navigation.goBack()}
-            style={styles.cancelButton}
-          >
-            <LinearGradient
-              colors={[colors.primary, colors.secondary]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 1 }}
-              style={styles.cancelButtonBorder}
-            >
-              <Text style={styles.cancelButtonText}>Avbryt</Text>
-            </LinearGradient>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.headerView}>
+        <Text style={styles.mainText}>Bildgalleri</Text>
+        <InfoModal screen="imageGallery" tooltipWidth={200} />
       </View>
+      <FlatList
+        style={styles.container}
+        columnWrapperStyle={{ marginBottom: 10 }}
+        data={imagesArray}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            testID="pressOnImage"
+            onPress={() => setImageName(item.name)}
+            style={{ flex: 0.5, flexDirection: "row" }}
+          >
+            <Image
+              testID="imageInImageGallery"
+              style={imageStyle(item.selected)}
+              source={item.image}
+            />
+          </TouchableOpacity>
+        )}
+      />
+      <BottomNavButtons
+        primaryText="Spara"
+        primaryFunc={buttonSavePressed}
+        secondaryText="Avbryt"
+        secondaryFunc={navigation.goBack}
+      />
     </SafeAreaView>
   );
 }
@@ -137,6 +116,7 @@ export default ImagesGallery;
 const styles = StyleSheet.create({
   headerView: {
     marginTop: 12,
+    paddingHorizontal: 16,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
@@ -146,46 +126,6 @@ const styles = StyleSheet.create({
   },
   container: {
     marginTop: 12,
-  },
-  containerForTwoBottomButtons: {
-    marginTop: 10,
-    marginBottom: 16,
-  },
-  saveButton: {
-    borderRadius: 5,
-    backgroundColor: colors.primary,
-    height: 55,
-    marginBottom: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  saveButtonText: {
-    ...typography.button.lg,
-    color: colors.dark,
-    fontWeight: "500",
-  },
-  cancelButton: {
-    borderRadius: 5,
-    backgroundColor: colors.light,
-    height: 55,
-  },
-  cancelButtonText: {
-    ...typography.button.lg,
-    backgroundColor: colors.light,
-    borderRadius: 5,
-    height: "100%",
-    width: "100%",
-    textAlign: "center",
-    textAlignVertical: "center",
-
-    color: colors.dark,
-    overflow: "hidden",
-    paddingTop: Platform.OS === "ios" ? 12 : null,
-    fontWeight: "500",
-  },
-  cancelButtonBorder: {
-    paddingVertical: 1,
-    paddingHorizontal: 1,
-    borderRadius: 5,
+    paddingHorizontal: 16,
   },
 });
