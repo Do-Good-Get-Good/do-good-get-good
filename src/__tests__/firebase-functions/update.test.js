e126f46c8003a22147d942964e01c4828daeca89
Mattan1999 <mattias470@gmail.com>
2023-03-31 10:55:05 +0200

    Update tests

--- src/__tests__/firebase-functions/update.test.js
+++ src/__tests__/firebase-functions/update.test.js
@@ -1,5 +1,3 @@
-import firestore from "@react-native-firebase/firestore";
-
 import { calculateNewHours } from "../../firebase-functions/update.js";
 
 jest.mock("@react-native-firebase/firestore", () => ({
@@ -11,20 +9,60 @@ jest.mock("@react-native-firebase/firestore", () => ({
 }));
 
 test("that saved hours are zero if removed time are greater than registered time", () => {
-  var uid = "12354";
   var hours = 4;
   var registeredTime = 2;
 
-  totalHours = calculateNewHours(hours, registeredTime);
+  totalHours = calculateNewHours(hours, registeredTime, "sub");
   expect(totalHours).toBe(0);
 });
 
 test("that saved hours are removed with the right amout", () => {
-  var uid = "12354";
   var hours = 2;
   var registeredTime = 4;
   var answer = registeredTime - hours;
 
-  totalHours = calculateNewHours(hours, registeredTime);
+  totalHours = calculateNewHours(hours, registeredTime, "sub");
   expect(totalHours).toBe(answer);
+});
+
+test("that saved hours are removed with the right amout even if amount is big", () => {
+  var hours = Number.MAX_SAFE_INTEGER;
+  var registeredTime = 2423;
+
+  totalHours = calculateNewHours(hours, registeredTime, "sub");
+  expect(totalHours).toBe(0);
+});
+
+test("that saved hours are added with the right amout", () => {
+  var hours = 2;
+  var registeredTime = 0;
+  var answer = registeredTime + hours;
+
+  totalHours = calculateNewHours(hours, registeredTime, "add");
+  expect(totalHours).toBe(answer);
+});
+
+test("that saved hours are added with the right amout even if amount is big", () => {
+  var hours = Number.MAX_SAFE_INTEGER;
+  var registeredTime = 2423;
+  var answer = registeredTime + hours;
+
+  totalHours = calculateNewHours(hours, registeredTime, "add");
+  expect(totalHours).toBe(answer);
+});
+
+test("that saved hours still is 0 when adding 0", () => {
+  var hours = 0;
+  var registeredTime = 0;
+
+  totalHours = calculateNewHours(hours, registeredTime, "add");
+  expect(totalHours).toBe(0);
+});
+
+test("that saved hours still is 0 when subtracting 0", () => {
+  var hours = 0;
+  var registeredTime = 0;
+
+  totalHours = calculateNewHours(hours, registeredTime, "sub");
+  expect(totalHours).toBe(0);
 });
