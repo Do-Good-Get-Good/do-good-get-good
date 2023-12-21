22e041a492f554889f5ed1bad7bee5bf9b683cd4
Mattan1999 <mattias470@gmail.com>
2023-12-21 13:07:18 +0100

    Change resize mode

--- src/components/HomeSuggestions.js
+++ src/components/HomeSuggestions.js
@@ -122,7 +122,7 @@ const styles = StyleSheet.create({
   image: {
     width: 100,
     height: 100,
-    resizeMode: "contain",
+    resizeMode: "cover",
     alignItems: "center",
     borderRadius: 5,
   },
