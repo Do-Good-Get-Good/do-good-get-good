ed57745725832b7c98fe4054f6d3250647e9afeb
Mattan1999 <mattias470@gmail.com>
2023-12-29 19:27:33 +0100

    Update UserLevelContext

---
+++ src/context/UserLevelContext.tsx
@@ -0,0 +1,25 @@
+import React, { useContext, PropsWithChildren } from "react";
+import { UserPermissionLevel } from "../utilily/types";
+
+type UserLevel = { userLevel: UserPermissionLevel };
+
+const UserLevelContext = React.createContext<UserPermissionLevel>({
+  superadmin: false,
+  admin: false,
+  user: false,
+});
+
+export const useUserLevel = () => {
+  return useContext(UserLevelContext);
+};
+
+export const UserLevelProvider = ({
+  userLevel,
+  children,
+}: PropsWithChildren<UserLevel>) => {
+  return (
+    <UserLevelContext.Provider value={userLevel}>
+      {children}
+    </UserLevelContext.Provider>
+  );
+};
