76db553cc8f7a3c5261a20f5af18ef87928daeb7
Mattan1999 <mattias470@gmail.com>
2023-12-30 01:36:19 +0100

    Add confirmation alert dialog

--- src/screens/DeveloperOptions/DeveloperOptions.tsx
+++ src/screens/DeveloperOptions/DeveloperOptions.tsx
@@ -1,31 +1,58 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-import Menu from "../../components/Menu";
-import colors from "../../assets/theme/colors";
 import functions from "@react-native-firebase/functions";
 
+import LoadingOverlay from "../../components/LoadingOverlay";
+import Menu from "../../components/Menu";
+import colors from "../../assets/theme/colors";
+import typography from "../../assets/theme/typography";
+
+const updateAllUsersPermissions = (setLoading: (value: boolean) => void) => {
+  Alert.alert(
+    "Flytta behörighet",
+    `Är du säker på att du vill flytta alla användares behörigheter till custom claims?`,
+    [
+      {
+        text: "Nej",
+        style: "destructive",
+      },
+      {
+        text: "Ja",
+        onPress: () => {
+          setLoading(true);
+          functions()
+            .httpsCallable("updateAllUsersPermissions")()
+            .then((res) => {
+              setLoading(false);
+              console.log(res);
+              Alert.alert(
+                "Flyttningen lyckades",
+                `Flyttningen av användares behörgiheter has slutförts!`,
+              );
+            })
+            .catch((error) => console.log(error));
+        },
+      },
+    ],
+  );
+};
+
 const DeveloperOptions = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <SafeAreaView style={styles.container}>
       <Menu />
       <View style={styles.contentWrapper}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => {
-            const updateAllUsersPermissions = functions().httpsCallable(
-              "updateAllUsersPermissions",
-            );
-
-            updateAllUsersPermissions()
-              .then((res) => console.log(res))
-              .catch((error) => console.log(error));
-          }}
+          onPress={() => updateAllUsersPermissions(setLoading)}
         >
-          <Text>Move users permissions to custom claims</Text>
+          <Text style={styles.buttonText}>Flytta användares behörigheter</Text>
         </TouchableOpacity>
       </View>
+      {loading && <LoadingOverlay />}
     </SafeAreaView>
   );
 };
@@ -50,5 +77,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.primary,
     display: "flex",
     alignItems: "center",
+  },
+  buttonText: {
+    ...typography.b2,
   },
 });
