15b30d196d0f0322086f6aba3819de1ed917cba3
Jaya <mavuleti.satya@gmail.com>
2024-03-19 12:37:08 +0100

    Commented chat screen

--- src/components/MenuOverlay/useMenuNavigation.ts
+++ src/components/MenuOverlay/useMenuNavigation.ts
@@ -32,10 +32,10 @@ export const userNavigations: Array<NavigationObject> = [
     title: "FAQ",
     screenName: UserStack.Faq,
   },
-  {
-    title: "Chat",
-    screenName: UserStack.Chat,
-  }
+  // {
+  //   title: "Chat",
+  //   screenName: UserStack.Chat,
+  // }
 ];
 
 export const useMenuNavigation = (role: Role | undefined) => {
