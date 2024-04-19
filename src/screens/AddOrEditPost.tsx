2c113d2aa67ee96132cc9bd6e1d8331d6174bbc7
Jaya <mavuleti.satya@gmail.com>
2024-04-19 16:48:06 +0200

    Adding ImagePicker library

--- src/screens/AddOrEditPost.tsx
+++ src/screens/AddOrEditPost.tsx
@@ -10,6 +10,8 @@ import { UserPost } from "../utility/types";
 import { LongButton } from "../components/Buttons/LongButton";
 import { GoBackButton } from "../components/Buttons/GoBackButton";
 import { ChatCardHeader } from "../components/ChartCard/ChatCardHeader";
+import ImagePicker, { launchImageLibrary } from 'react-native-image-picker';
+
 
 type Props = {
   route: any;
@@ -23,6 +25,30 @@ type Params = {
 export const AddOrEditPost = ({route,navigation}:Props) => {
     const {post, toEdit  }:Params = route.params;
   const [text, setText] = useState(post.description);
+  const [image, setImage] = useState('');
+
+  const handleImagePicker = () => {
+    const options = {
+      mediaType: 'photo' as const, // Ensure mediaType is of type 'photo'
+      maxWidth: 300,
+      maxHeight: 300,
+    };
+  
+    ImagePicker.launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.errorCode) { // Check for errorCode instead of error
+        console.log('ImagePicker Error Code: ', response.errorCode);
+        if (response.errorMessage) {
+          console.log('ImagePicker Error Message: ', response.errorMessage);
+        }
+      } else {
+        console.log('Image URI is missing in response');
+        // Handle image selection here
+      }
+    });
+  };
+  
   
   return (
     <SafeAreaView style={styles.container}>
@@ -30,7 +56,7 @@ export const AddOrEditPost = ({route,navigation}:Props) => {
       <GoBackButton/>
       <ScrollView>
       <ChatCardHeader post={post} />
-      <TouchableOpacity onPress={()=>{}}>
+      <TouchableOpacity onPress={handleImagePicker}>
           <View style={styles.image}>
             <Text style={styles.imageText}>Lägga till bild</Text>
             <Text  style={styles.buttonText }>+</Text>
