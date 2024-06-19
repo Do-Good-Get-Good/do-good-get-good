8504d40ec345426c12273c1f23ebd29f7a9a6b80
Alona-Kirichenko <alena1412199217@gmail.com>
2024-06-19 13:57:36 +0200

    Thinking what to do with function

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
-// const admin = require("firebase-admin");
 const { initializeApp } = require("firebase-admin/app");
 const { createUser } = require("./src/createUser");
 const { deleteUser } = require("./src/deleteUser");
 const { updateMonth } = require("./src/updateMonth");
 const { updateYear } = require("./src/updateYear");
-const { assignAdminClaim } = require("./src/assignAdminClaim");
+// const { assignAdminClaim } = require("./src/assignAdminClaim");
 const { downloadData } = require("./src/downloadData/downloadData");
 const { connectUsersToActivities } = require("./src/connectUsersToActivities");
 const { updateActivityUserCount } = require("./src/updateActivityUserCount");
@@ -14,7 +13,6 @@ const {
 } = require("./src/adminGetUserEmailSecondGen");
 const { updateUser } = require("./src/updateUser");
 const { deletePostAfterOneYear } = require("./src/deletePostAfterOneYear");
-// admin.initializeApp(firebaseConfig);
 initializeApp(firebaseConfig);
 
 // Cloud functions
@@ -22,7 +20,7 @@ exports.createUser = createUser;
 exports.deleteUser = deleteUser;
 exports.updateMonth = updateMonth;
 exports.updateYear = updateYear;
-exports.assignAdminClaim = assignAdminClaim;
+// exports.assignAdminClaim = assignAdminClaim;
 exports.downloadData = downloadData;
 exports.connectUsersToActivities = connectUsersToActivities;
 exports.updateActivityUserCount = updateActivityUserCount;
