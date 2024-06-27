5276c322e3a3681b1884f6155e66d77983253fda
Alona-Kirichenko <alena1412199217@gmail.com>
2024-06-27 09:36:21 +0200

    Changing updateActivityUserCountSecondGen to second gen

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,36 @@
 const { initializeApp } = require("firebase-admin/app");
 // const { createUser } = require("./src/createUser");
 const { createUserSecondGen } = require("./src/createUserSecondGen");
-const { deleteUser } = require("./src/deleteUser");
 const { updateMonth } = require("./src/updateMonth");
 const { updateYear } = require("./src/updateYear");
-// const { assignAdminClaim } = require("./src/assignAdminClaim");
 const { downloadData } = require("./src/downloadData/downloadData");
-// const { connectUsersToActivities } = require("./src/connectUsersToActivities");
 const {
   connectUsersToActivitiesSecondGen,
 } = require("./src/connectUsersToActivitiesSecondGen");
-const { updateActivityUserCount } = require("./src/updateActivityUserCount");
 const { firebaseConfig } = require("./config");
 const {
   adminGetUserEmailSecondGen,
 } = require("./src/adminGetUserEmailSecondGen");
 const { updateUser } = require("./src/updateUser");
-// const { deletePostAfterOneYear } = require("./src/deletePostAfterOneYear");
 const {
   deletePostAfterOneYearSecondGen,
 } = require("./src/deletePostAfterOneYearSecondGen");
 initializeApp(firebaseConfig);
+const {
+  updateActivityUserCountSecondGen,
+} = require("./src/updateActivityUserCountSecondGen");
 
 // Cloud functions
 // exports.assignAdminClaim = assignAdminClaim;
+// exports.deleteUser = deleteUser;
 
 exports.createUserSecondGen = createUserSecondGen;
 exports.deletePostAfterOneYearSecondGen = deletePostAfterOneYearSecondGen;
 exports.connectUsersToActivitiesSecondGen = connectUsersToActivitiesSecondGen;
 exports.adminGetUserEmailSecondGen = adminGetUserEmailSecondGen;
+exports.updateActivityUserCountSecondGen = updateActivityUserCountSecondGen;
 
-exports.deleteUser = deleteUser;
 exports.updateMonth = updateMonth;
 exports.updateYear = updateYear;
-
 exports.downloadData = downloadData;
-
-exports.updateActivityUserCount = updateActivityUserCount;
-
 exports.updateUser = updateUser;
