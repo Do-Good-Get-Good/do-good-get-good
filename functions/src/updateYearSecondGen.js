0460533d3e21000b45530648dc9c4260ea43ec97
Alona-Kirichenko <alena1412199217@gmail.com>
2024-08-27 14:29:42 +0200

    updating schedule functions. now they are running on europe-west1

--- functions/src/updateYearSecondGen.js
+++ functions/src/updateYearSecondGen.js
@@ -1,18 +1,22 @@
 const { onSchedule } = require("firebase-functions/v2/scheduler");
 const { getFirestore } = require("firebase-admin/firestore");
 
-exports.updateYearSecondGen = onSchedule("0 0 1 1 *", async (event) => {
-  try {
-    await getFirestore()
-      .collection("Users")
-      .get()
-      .then((snap) => {
-        snap.forEach((doc) => {
-          doc.ref.update({ total_hours_year: 0 });
+// onSchedule don't suport region europe-north1.  More info at README
+exports.updateYearSecondGen = onSchedule(
+  { schedule: "0 0 1 1 *", region: "europe-west1" },
+  async (event) => {
+    try {
+      await getFirestore()
+        .collection("Users")
+        .get()
+        .then((snap) => {
+          snap.forEach((doc) => {
+            doc.ref.update({ total_hours_year: 0 });
+          });
         });
-      });
-    return console.log("Successful reset of time for year");
-  } catch (error) {
-    console.error("Error fetching user data:", error);
+      return console.log("Successful reset of time for year");
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
   }
-});
+);
